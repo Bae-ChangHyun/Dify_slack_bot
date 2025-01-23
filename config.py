420abde321b439920f0fe8375637ef20cf1c79d1
chbae <chbae@gcsc.co.kr>
2025-01-23 14:02:09 +0900

    Feat: add Slack signing secret and app token to configuration
    - Added `slack_signing_secret` and `slack_app_token` environment variables to the configuration file for enhanced Slack app security and functionality.

--- config.py
+++ config.py
@@ -16,6 +16,8 @@ dify_base_url = os.getenv('dify_base_url')
 # Slack variables
 slack_base_url = os.getenv('slack_base_url')
 slack_web_hook = os.getenv('slack_web_hook')
+slack_signing_secret = os.getenv('slack_signing_secret')
+slack_app_token = os.getenv('slack_app_token')
 slack_OAuth_token = os.getenv('slack_OAuth_token')
 
 logger = CustomLogger("chat_log")
