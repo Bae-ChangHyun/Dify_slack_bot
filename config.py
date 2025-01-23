a87c0d7b219973a89c6664e33f82e33f58383529
chbae <chbae@gcsc.co.kr>
2025-01-23 16:30:17 +0900

    config: add Redis configuration variables
    - Introduced environment variables for Redis connection: `redis_host`, `redis_port`, `redis_db`, and `redis_password` to enhance configuration flexibility and support for Redis integration.

--- config.py
+++ config.py
@@ -20,4 +20,10 @@ slack_signing_secret = os.getenv('slack_signing_secret')
 slack_app_token = os.getenv('slack_app_token')
 slack_OAuth_token = os.getenv('slack_OAuth_token')
 
+# Redis variables
+redis_host = os.getenv('redis_host')
+redis_port = os.getenv('redis_port')
+redis_db = os.getenv('redis_db')
+redis_password = os.getenv('redis_password')
+
 logger = CustomLogger("chat_log")
